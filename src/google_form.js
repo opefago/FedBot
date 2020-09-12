466e65c2c231a73e16018d20ce60e0140d416e3a
opefago <damilola.fagoyinbo@gmail.com>
2020-09-12 19:49:49 +0300

    Configure File for google credentials

--- src/google_form.js
+++ src/google_form.js
@@ -1,7 +1,7 @@
 const { GoogleSpreadsheet } = require('google-spreadsheet');
 const doc = new GoogleSpreadsheet(process.env.GOOGLE_SHEET_ID);
 module.exports.parseSheet = async function run(callback){
-    await doc.useServiceAccountAuth(require('./credentials.json'));
+    await doc.useServiceAccountAuth(require(process.env.GOOGLE_KEY_FILE));
     await doc.loadInfo();
     const sheet = doc.sheetsByIndex[0];
     const rows = await sheet.getRows();
